f062dff789abacdaa99be96b3b3c61f782871ad3
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-01 02:17:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now.hour < 12 
-    resp.write "Good Morning!" 
-    else 
-    resp.write "Good Afternoon!"
+    # if Time.now.hour < 12 
+    # resp.write "Good Morning!" 
+    # else 
+    # resp.write "Good Afternoon!"
     
     resp.finish
     end 
